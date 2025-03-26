f65a5bc147e21260b51ac71765cca19b66de5212
Michał Pietrucha <mpietrucha50@gmail.com>
2025-03-26 12:45:07 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 export { is, is as isNone, not, not as notNone } from '@/is'
-export { None, none as createNone, none } from '@/none'
+export { None, none } from '@/none'
--- src/is.js
+++ src/is.js
@@ -1,9 +1,9 @@
 import { None } from '@/none'
-import { isConstructor } from '@mpietrucha/is-constructor'
+import { isInstanceOf } from '@mpietrucha/is-constructor'
 import { negate } from 'lodash-es'
 
 export const is = value => {
-    return isConstructor(value, None)
+    return isInstanceOf(value, None)
 }
 
 export const not = negate(is)
